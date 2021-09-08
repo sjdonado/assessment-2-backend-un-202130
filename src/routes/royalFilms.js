870c6c86a775138c0cec745cb563db853219339d
Giovanoti <cgmoreno@uninorte.edu.co>
2021-09-08 18:15:46 -0500

    feat: formato json, obtención de las peliculas mostradas

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -1,14 +1,15 @@
 const scraper = require('../services/scraper');
 
 async function get(req, res) {
-	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
-	} catch(err) {
-		res.writeJSONResponse({ data: null, err: err.message }, 500);
-	}
+    try {
+        const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
+        const pageBody = await scraper.getPageBody('https://royal-films.com/cartelera/barranquilla');
+        res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: pageBody } }, 200);
+    } catch (err) {
+        res.writeJSONResponse({ data: null, err: err.message }, 500);
+    }
 }
 
 module.exports = {
-	get,
+    get,
 };
