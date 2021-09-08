ba8d4246059fd9a869193e83869f6a83c4fe25d9
TheLokestraps <ivanestebanperezdlo@hotmail.com>
2021-09-08 02:16:22 +0000

    feat: scraper.js, route and readme.md

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,11 +3,49 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const Urls = await scraper.getAllURLMovies('https://royal-films.com/cartelera/barranquilla');
+		let originalTitle,title,synopsis,starred,director,posterPhoto,trailer,Aux;
+		let Mov = [];
+		for(var i = 0; i < Urls.length; i++){
+			console.log(Urls[i]);
+			Aux = await scraper.getMovieTextInfo(Urls[i]);
+			console.log(Aux);
+			title = Aux[0];
+			originalTitle = Aux[1];
+			starred = Aux[2];
+			director = Aux[3];
+			synopsis = Aux[4];
+			posterPhoto = await scraper.getImage(Urls[i]);
+			trailer = await scraper.getVideo(Urls[i]);
+			Mov.push({originalTitle ,title, synopsis ,starred,director,posterPhoto,trailer});
+			
+		};
+
+		let AllMovieInfo = await Promise.all(Mov);
+		res.writeJSONResponse({ data: { 'pageTitle' : pageTitle, 'AllMovieInfo' : AllMovieInfo} }, 200);
+
+
+
+
+		/* console.log(Urls);
+		const Aux = await scraper.getMovieTextInfo(Urls[0]);
+		let originalTitle,title,synopsis,starred,director,posterPhoto,trailer;
+		originalTitle = Aux[0];
+		title = Aux[1];
+		synopsis = Aux[2]
+		starred = Aux[3]
+		director = Aux[4]
+		posterPhoto = await scraper.getImage(Urls[0]);
+		trailer = await scraper.getVideo(Urls[0]);
+		console.log("end");
+		AllMovieInfo = {originalTitle,title,synopsis,starred,director,posterPhoto,trailer};
+		res.writeJSONResponse({ data: { pageTitle, AllMovieInfo} }, 200);
+ */
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
 }
+
 
 module.exports = {
 	get,
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -17,6 +17,79 @@ async function getPageTitle(url) {
 	return title;
 }
 
+async function getMovieTextInfo(url){
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setRequestInterception(true);
+	page.on('request', (req) => {
+	  if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+		req.abort();
+	  }
+	  else {
+		req.continue();
+	  }
+	});
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('div > div > div> div > div > div > ul > li > span');
+	try{
+		await page.click('div > div > div > p > a');
+	} catch (error){
+		console.log('No tiene ver mas')
+	};
+	const Descripcion = await page.evaluate(() => document.querySelector('div > div > div > div > div> p').innerText);
+  
+	const Tabla = await page.evaluate(() => Array.from(document.querySelectorAll('table > tbody > tr > td'), element => element.innerText));
+  
+	const Titulo = String(Tabla[0]);
+	const Original = String(Tabla[1]);
+	const Reparto = String(Tabla[2]);
+	const Director = String(Tabla[3]);
+	await page.screenshot({ path: 'example1.png' });
+	await browser.close();
+  
+	const AllMovieInfo = [Titulo,Original,Reparto,Director,Descripcion];
+	
+	return AllMovieInfo;
+
+}
+async function getVideo(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('div > div > div> div > div > div > ul > li > span');
+	await page.click('#header > div:nth-child(3) > span');
+	await page.waitForSelector('iframe');
+	const Video = await page.evaluate(() => document.querySelector('iframe').src);
+	await browser.close();
+  
+	 return Video;
+  }
+
+	  async function getImage(url) {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(url, { waitUntil: 'networkidle0' });
+		await page.waitForSelector('div > div > div> div > div > div > ul > li > span');
+		const Image = await page.evaluate(() => document.querySelector('#movie > div > div > div> div > span').style.backgroundImage);
+		let urlimage = String(Image);
+		urlimage = urlimage.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+		await browser.close();
+	
+		return urlimage;
+	  }
+
+  async function getAllURLMovies(url){
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('div > div > div > div > a');
+	const AllUrl = await page.evaluate(() => Array.from(document.querySelectorAll('div > div > div > div > div > a'), element => element.href));
+
+	return AllUrl;
+  
+  }
+
+
 module.exports = {
-	getPageTitle,
+	getPageTitle,getAllURLMovies,getImage,getVideo,getMovieTextInfo
 };
