3d475ef000bc93b83b9a49af06323de32355b857
QuantumGorilla <manotasja@uninorte.edu.co>
2021-09-08 03:35:47 +0000

    fix: debug royalfilms.js

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -1,15 +1,28 @@
 const scraper = require('../services/scraper');
 
 async function get(req, res) {
-	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		const pageBody = await scraper.getPageBody('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle , allMoviesDetails: pageBody} }, 200);
-	} catch(err) {
-		res.writeJSONResponse({ data: null, err: err.message }, 500);
-	}
+    try {
+        const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla')
+        const pageData = await scraper.getData('https://royal-films.com/cartelera/barranquilla')
+
+        const datas=[]
+
+        for (i of pageData) {
+            x= scraper.data(i)
+            datas.push(x) 
+
+          }
+          let values = await Promise.all(datas);
+        const dataJson = {
+            pageTitle: pageTitle,
+            allMoviesDetails:values
+        }
+        res.writeJSONResponse({ data: dataJson }, 200);
+    } catch(err) {
+        res.writeJSONResponse({ data: null, err: err.message }, 500);
+    }
 }
 
 module.exports = {
-	get,
+    get,
 };
