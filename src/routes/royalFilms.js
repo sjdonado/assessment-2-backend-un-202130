ecd52c2661ae2b08248f128bf41e290357bea2f9
sdariza <sdariza@uninorte.edu.co>
2021-09-08 02:25:41 +0000

    feat: obtiene la url de cada pelicula

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,8 +3,11 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
-	} catch(err) {
+		const allMoviesDetails = await scraper.getPageInfo('https://royal-films.com/cartelera/barranquilla')
+		res.writeJSONResponse({
+			data: { pageTitle, allMoviesDetails }
+		}, 200);
+	} catch (err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
 }
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -6,17 +6,30 @@ const puppeteer = require('puppeteer');
  * @returns {string}
  */
 async function getPageTitle(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.goto(url, { waitUntil: 'networkidle0' });
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
 	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
-
 	await browser.close();
-
 	return title;
 }
 
+async function getPageInfo(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('.movie-box') // wait for the element
+	let element = await page.$$('.movie-box') // return all the elements with the class style given
+	let urls = [];
+	for (const x of element) {//  pathway taken by the elements 
+		urls.push('https://royal-films.com/' + await page.evaluate(el => el.getAttribute('href'), x))
+	}
+	console.log(urls)
+	return urls
+
+}
+
+
 module.exports = {
-	getPageTitle,
+	getPageTitle, getPageInfo,
 };
