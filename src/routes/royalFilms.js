378ca3a168cb3b7bf5528474c4a45a465d6b5467
Andres <50928089+Andresifico@users.noreply.github.com>
2021-09-09 08:52:24 -0500

    ci(scraper.js): add method get movie data

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -2,8 +2,14 @@ const scraper = require('../services/scraper');
 
 async function get(req, res) {
 	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const pageTitle = await scraper.Get_Page_Title('https://royal-films.com/cartelera/barranquilla');
+		const pageInfo = await scraper.Get_INFORMATION('https://royal-films.com/cartelera/barranquilla');
+        let prom = await Promise.all(pageInfo);
+		const Json = {
+			pageTitle: pageTitle,
+			allMoviesDetails: prom
+		}
+		res.writeJSONResponse({ data: { Json } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
