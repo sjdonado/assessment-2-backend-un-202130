5168756e58da44faa08d017c075f1990617bd082
rmanuelj01 <rmanuelj@uninorte.edu.co>
2021-09-08 23:47:01 -0500

    feat: scraper.js, royalFilms.js and src/utils/index.js

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const pageBody = await scraper.getPageBody('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle , allMoviesDetails: pageBody} }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
