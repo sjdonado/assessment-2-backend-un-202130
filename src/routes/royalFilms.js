4a6393c61a764e3a4ca8fd20c64e4616d5128fb4
jdlagares <sportymammal@gmail.com>
2021-09-08 22:39:17 -0500

    fix: Areglo de funcion para retornar toda la data

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const Movedetails= await scraper.getMoviesDetails('https://royal-films.com/cartelera/barranquilla')
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: Movedetails  } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -16,7 +16,49 @@ async function getPageTitle(url) {
 
 	return title;
 }
+async function getMoviesDetails(url){
+	const browser = await puppeteer.launch({headless: false});
+  	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('.movie-box');
+	const urls = await page.evaluate(()=>{
+		const elements=document.querySelectorAll('.movie-box');
+		const links=[]
+		for(let element of elements){
+			links.push(element.href);
+		}
+		return links;
+	})
+	const movies=[]
+	for( let a of urls){
+		await page.goto(a);
+		await page.waitForTimeout('3000')
+		await page.click('[class="img gaussian"]')
+		const data = await page.evaluate(()=>{
+			const details={}
+			details.originalTitle=document.querySelector('.people > tbody > tr:nth-child(2) > td').innerText
+			details.title=document.querySelector('.people tbody tr:nth-child(1) td').innerText
+			details.synopsis=document.querySelector('.synopsis').innerText
+			details.starred=document.querySelector('.people tbody tr:nth-child(3) td').innerText
+			details.director=document.querySelector('.people tbody tr:nth-child(4) td').innerText
+			details.postherPhoto=document.querySelector('#movie > div > div > div.col-xl-3.col-lg-4.col-md-5.col-sm-12 > div > span').style.backgroundImage.slice(4, -1).replace(/"/g, "")
+			const trailer=document.getElementById('player').children[0].children[0].src
+			details.trailer='https://www.youtube.com/watch?v='+trailer.substring(30,trailer.indexOf('?'))
+
+			return details
+			
+			//#movie > div > div > div.col-xl-9.col-lg-8.col-md-7.col-sm-12 > div > div:nth-child(2) > table > tbody > tr:nth-child(2) > td
+		})
+		movies.push(data)
+	}
+	console.log(movies)
+	await browser.close();
+	return movies;
+	  
+}
+
 
 module.exports = {
 	getPageTitle,
+	getMoviesDetails,
 };
