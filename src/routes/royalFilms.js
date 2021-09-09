97e37e3953ab619924a5ac27744b9613309bbeb2
Kang Lei <leik@uninorte.edu.co>
2021-09-09 07:53:59 +0000

    ci(royalFilms.js): Add the Json variable to be able to display results

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -2,8 +2,19 @@ const scraper = require('../services/scraper');
 
 async function get(req, res) {
 	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const pageTitle = await scraper.Get_Page_Title('https://royal-films.com/cartelera/barranquilla');
+		const pageInfo = await scraper.Get_INFORMATION('https://royal-films.com/cartelera/barranquilla');
+		let i=0
+		const list= []
+		while(i<pageInfo.length){
+            list.push(pageInfo[i])
+			i++;
+		}
+		const Json = {
+			pageTitle: pageTitle,
+			allMoviesDetails: pageInfo
+		}
+		res.writeJSONResponse({ data: { Json } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
