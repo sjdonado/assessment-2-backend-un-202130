2361fb4ef5bd5a0bb9bc1c8feac81020abdcc387
Dolcey Mendoza Jr <dolceymendozajr@gmail.com>
2021-09-09 08:52:29 -0500

    feat: ✨get movies details
    update dependencies, add feat to see movies details

--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const puppeteer = require('puppeteer');
 
 /**
@@ -5,18 +6,47 @@ const puppeteer = require('puppeteer');
  * @param {string} url
  * @returns {string}
  */
-async function getPageTitle(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+async function getPageData(url) {
+    const browser = await puppeteer.launch();
 
-  await page.goto(url, { waitUntil: 'networkidle0' });
-	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
+    const page = await browser.newPage();
 
-	await browser.close();
+    await page.goto(url, { waitUntil: 'networkidle0' });
 
-	return title;
+    const pageTitle = await page.evaluate(() => document.querySelector('head > title').innerText);
+
+    await page.waitForSelector('.movie-box');
+    const allMoviesDetails = [];
+    const endpoints = await page.evaluate(() => {
+        const movieBoxes = document.querySelectorAll('.movie-box')
+        const endpoints = [];
+        movieBoxes.forEach((movieBox) => {
+            const movieId = (movieBox.href.match(/\d/g)).join("");
+            endpoints.push(`https://royal-films.com/api/v1/movie/${movieId}/barranquilla`);
+        })
+        return endpoints;
+    });
+
+    await browser.close();
+
+    for (const endpoint of endpoints) {
+        await axios.get(endpoint).then(response => {
+            const movieData = response.data.data;
+            allMoviesDetails.push({
+                'originalTitle': movieData.original,
+                'title': movieData.title,
+                'synopsis': movieData.synopsis,
+                'starred': movieData.starred,
+                'director': movieData.director,
+                'posterPhoto': movieData.poster_photo,
+                'trailer': `https://youtu.be/${movieData.youtube}`
+            });
+        });
+    }
+
+    return { pageTitle, allMoviesDetails };
 }
 
 module.exports = {
-	getPageTitle,
+    getPageData
 };
