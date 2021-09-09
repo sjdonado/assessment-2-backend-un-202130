73d12ef870b6c364effb6940e5137cc29f7da49b
David Figueroa <davidfigueroa0327@gmail.com>
2021-09-09 06:13:58 -0500

    feat: scraping movies information

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -1,14 +1,14 @@
 const scraper = require('../services/scraper');
 
 async function get(req, res) {
-	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
-	} catch(err) {
-		res.writeJSONResponse({ data: null, err: err.message }, 500);
-	}
+  try {
+    const { title, moviesData } = await scraper.getMoviesInfo('https://royal-films.com/cartelera/barranquilla');
+    res.writeJSONResponse({ data: { pageTitle: title, allMoviesDetails: moviesData } }, 200);
+  } catch (err) {
+    res.writeJSONResponse({ data: null, err: err.message }, 500);
+  }
 }
 
 module.exports = {
-	get,
+  get,
 };
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -3,20 +3,38 @@ const puppeteer = require('puppeteer');
 /**
  * Go to url and return the page title
  * @param {string} url
- * @returns {string}
+ * @returns {Promise<{title: string, movies: any[]}>}
  */
-async function getPageTitle(url) {
+async function getMoviesInfo(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'networkidle0' });
-	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
+  const title = await page.evaluate(() => document.querySelector('head > title').innerText);
 
-	await browser.close();
+  const moviesDataPromises = await page.evaluate(() => {
+    const links = Array.from(document.querySelectorAll('.movie-box')).map(el => el.href);
+    const movieIds = links.map(link => (new URL(link).pathname.split('/')[3]));
+    const resp = movieIds.map(movieId => fetch(`https://royal-films.com/api/v1/movie/${movieId}/barranquilla`).then(res => res.json()));
 
-	return title;
+    return Promise.all(resp);
+  });
+
+  const moviesData = (await moviesDataPromises).map(movie => ({
+    originalTitle: movie.data.original,
+    title: movie.data.title,
+    synopsis: movie.data.synopsis,
+    starred: movie.data.starred,
+    director: movie.data.director,
+    posterPhoto: movie.data.poster_photo,
+    trailer: `https://youtube.com/watch?v=${movie.data.youtube}`,
+  }));
+
+  await browser.close();
+
+  return { title, moviesData };
 }
 
 module.exports = {
-	getPageTitle,
+  getMoviesInfo,
 };
