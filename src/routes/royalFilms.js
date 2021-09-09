e2ee97b106b7cf71969a2f902ccf321c79c7ca71
Giovanoti <cgmoreno@uninorte.edu.co>
2021-09-08 20:31:08 -0500

    fix: debug pagebody

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -4,7 +4,7 @@ async function get(req, res) {
     try {
         const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
         const pageBody = await scraper.getPageBody('https://royal-films.com/cartelera/barranquilla');
-        res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: pageBody } }, 200);
+        res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: [pageBody] } }, 200);
     } catch (err) {
         res.writeJSONResponse({ data: null, err: err.message }, 500);
     }
