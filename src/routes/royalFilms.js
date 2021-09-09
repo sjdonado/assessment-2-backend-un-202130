5cdb41e25257ba30efa17c2da46c332cbdf5751d
Ronaldo Carreño Miranda <ronaldoc@uninorte.edu.co>
2021-09-09 18:51:00 -0500

    refactor: royalFilms
    allMoviesDetails

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -2,8 +2,11 @@ const scraper = require('../services/scraper');
 
 async function get(req, res) {
 	try {
+		const allMoviesDetails = await scraper.getMovieName('https://royal-films.com/cartelera/barranquilla');
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		res.writeJSONResponse({ data: { pageTitle, allMovieDetails:{
+			allMoviesDetails
+		} } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
