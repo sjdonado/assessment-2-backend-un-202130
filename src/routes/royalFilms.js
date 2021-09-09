0b8f0dbbd4a7bb663b5ec5957b7ff4395f61d47c
jdlagares <sportymammal@gmail.com>
2021-09-09 10:19:26 -0500

    fit: Agregar promesas en royalFilms.js

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -4,7 +4,12 @@ async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
 		const Movedetails= await scraper.getMoviesDetails('https://royal-films.com/cartelera/barranquilla')
-		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: Movedetails } }, 200);
+		let moviesInfo= await Promise.all(Movedetails)
+		const Info ={
+			pageTitle: pageTitle,
+			allMoviesDetails: moviesInfo
+		}
+		res.writeJSONResponse({ data:Info}, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
