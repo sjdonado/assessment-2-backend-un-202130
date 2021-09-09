14f8ac5a852a079b5882e23965b8e232b0d6a33d
Ubuntu <azureuser@Backend.rli3kbspb4aerm5dao1xaxgucg.bx.internal.cloudapp.net>
2021-09-09 15:20:23 +0000

    feat: return links

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const MoviesDetails = await scraper.getAllMoviesDetails('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: MoviesDetails } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -17,6 +17,33 @@ async function getPageTitle(url) {
 	return title;
 }
 
+
+async function getAllMoviesDetails(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+  
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('[class="my-3 col-lg-2 col-md-3 col-sm-4 col-6"] a');
+	const links = await page.evaluate(() => {
+        const elements = document.querySelectorAll('[class="my-3 col-lg-2 col-md-3 col-sm-4 col-6"] a');
+        const links = [];
+        for (let link of elements) {
+            links.push(link.href);
+        }
+        return links;
+    });
+	 /*const data = [];
+	 for (let x in links){
+		await page.goto(x, { waitUntil: 'networkidle0' });
+		const content = [];
+		content.title = await page.evaluate(() => data.push(document.querySelector('head > title').innerText));
+	}*/
+	await browser.close();
+	return links
+  }
+
+
 module.exports = {
 	getPageTitle,
+	getAllMoviesDetails,
 };
