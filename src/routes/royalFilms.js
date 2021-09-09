b32cae004c0472f2f8d830e0cffa59f872c4d6ae
jdlagares <sportymammal@gmail.com>
2021-09-08 22:44:09 -0500

    fix:arreglo de logica en scraper.js

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -4,7 +4,7 @@ async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
 		const Movedetails= await scraper.getMoviesDetails('https://royal-films.com/cartelera/barranquilla')
-		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: Movedetails  } }, 200);
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: Movedetails } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
