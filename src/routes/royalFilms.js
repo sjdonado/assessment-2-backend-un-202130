9a6d213d299e84625d6f3510c8410926df42a941
rmanuelj01 <rmanuelj@uninorte.edu.co>
2021-09-09 00:27:55 -0500

    fix: workflow require

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -4,8 +4,8 @@ async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
 		const pageBody = await scraper.getPageBody('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle , allMoviesDetails: pageBody} }, 200);
-	} catch(err) {
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails: pageBody } }, 200);
+	} catch (err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
 }
