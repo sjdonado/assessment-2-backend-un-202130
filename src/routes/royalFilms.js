d09309b1ff7e672ff9eef01a4afef5bd2a91d68d
Ubuntu <azureuser@Backend-Eval2.hcqbgbqd5coupid5ynsor4hqxc.jx.internal.cloudapp.net>
2021-09-09 04:04:51 +0000

    Envio imcompleto, actualizacion en curso, problemas con id

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const pageTitle2 = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle, pageTitle2 } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
