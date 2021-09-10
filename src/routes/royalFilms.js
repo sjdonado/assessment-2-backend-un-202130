33529d98083e711eeb11865b134f95a1f600ed86
Leonardo Archila <leo@elemento43.com>
2021-09-09 23:32:20 -0500

    feat: allMoviesDetails returned with pageTitle

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -2,8 +2,8 @@ const scraper = require('../services/scraper');
 
 async function get(req, res) {
 	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const {pageTitle, allMoviesDetails} = await scraper.getMoviesInfo('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,22 +1,52 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 /**
  * Go to url and return the page title
  * @param {string} url
- * @returns {string}
+ * @returns {Promise<{pageTitle: string, movies: any[]}>}
  */
-async function getPageTitle(url) {
+async function getMoviesInfo(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: 'networkidle0' });
-	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
+  await page.goto(url, { waitUntil: "networkidle0" });
 
-	await browser.close();
+  const pageTitle = await page.evaluate(
+    () => document.querySelector("head > title").innerText
+  );
 
-	return title;
+  const moviesDataPromises = await page.evaluate(() => {
+    const movieslinks = Array.from(
+      document.querySelectorAll(".movie-box"),
+      (element) => element.href
+    );
+    const movieIds = movieslinks.map(
+      (link) => new URL(link).pathname.split("/")[3]
+    );
+    const movies = movieIds.map((movieId) =>
+      fetch(`https://royal-films.com/api/v1/movie/${movieId}/barranquilla`)
+        .then((res) => res.json())
+        .then((res) => res.data)
+    );
+
+    return Promise.all(movies);
+  });
+
+  const allMoviesDetails = await moviesDataPromises.map((movie) => ({
+    originalTitle: movie.original,
+    title: movie.title,
+    synopsis: movie.synopsis,
+    starred: movie.starred,
+    director: movie.director,
+    posterPhoto: movie.poster_photo,
+    trailer: `https://youtube.com/watch?v=${movie.youtube}`,
+  }));
+
+  await browser.close();
+
+  return { pageTitle, allMoviesDetails };
 }
 
 module.exports = {
-	getPageTitle,
+  getMoviesInfo,
 };
