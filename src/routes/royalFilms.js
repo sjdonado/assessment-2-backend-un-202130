ba9080acccfa513cb3a40b34bb3270a17fff08a8
MRmario2411 <mariou@uninorte.edu.co>
2021-09-09 22:47:43 -0500

    build: Se crearon las fuciones getMoviesDate y getCaracteristics en el archivo scraper.js y dese modificó la función getDetails del archivo royalFilms.js

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -1,14 +1,25 @@
 const scraper = require('../services/scraper');
 
-async function get(req, res) {
-	try {
-		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
-	} catch(err) {
-		res.writeJSONResponse({ data: null, err: err.message }, 500);
-	}
+async function getDetails(req, res) {
+    try {
+        const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
+        const pageData = await scraper.getData('https://royal-films.com/cartelera/barranquilla');
+        const caracteristics=[]
+        for (m of pageData) {
+            car= scraper.details(m)
+            caracteristics.push(car) 
+          }
+          let information = await Promise.all(caracteristics);
+            const dataJson = {
+            pageTitle: pageTitle,
+            allMoviesDetails:information
+            }
+        res.writeJSONResponse({ data: dataJson }, 200);
+    } catch(err) {
+        res.writeJSONResponse({ data: null, err: err.message }, 500);
+    }
 }
 
 module.exports = {
-	get,
+    getDetails,
 };
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -10,13 +10,64 @@ async function getPageTitle(url) {
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'networkidle0' });
-	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
+    const title = await page.evaluate(() => document.querySelector('head > title').innerText);
 
-	await browser.close();
+    await browser.close();
 
-	return title;
+    return title;
 }
 
+async function getMoviesData(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+  await page.waitForSelector('[class=movie-box]')
+	const movie = await page.evaluate(() => { 
+		const movies = document.querySelectorAll('[class="movie-box"]')
+		const urls = []
+		movies.forEach((movies) => {
+			urlM= movies.getAttribute("href")
+      const pos = urlM.split("/") 
+      const cod = pos[pos.length-2]
+      direction="https://royal-films.com/api/v1/movie/"+cod+"/barranquilla?"
+      urls.push(direction)
+      })
+		return urls	
+	  })
+     
+  await browser.close();
+    return movie;
+    
+}
+
+async function getcaracteristics(url){
+  try {
+    const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+    const caracteristics = await page.evaluate(async (url) => {
+      const movie = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+        mode: "cors",
+      });
+      return movie.json();
+    },url);
+  await browser.close(); 
+  const AllCaracteristics = {originalTitle:caracteristics.data['original'],title:caracteristics.data['title'],synopsis:caracteristics.data['synopsis'],starred:caracteristics.data['starred'],director:caracteristics.data['director'],posterPhoto: "/"+caracteristics.data['poster_photo']+"/",
+  trailer: "https://youtube.com/watch?v="+caracteristics.data.youtube+"/",}
+  return { AllCaracteristics
+}
+  } catch (error) {
+    
+  }
+  
+
+}
 module.exports = {
-	getPageTitle,
+    getPageTitle,
+    getMoviesData,
+    getcaracteristics,
 };
