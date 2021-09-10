bd8da87b9cd948ca1486edc3dfb97110c39749e9
Alejandroo Gonzalez <aegs8002@gmail.com>
2021-09-10 02:33:04 +0000

    feat: agregar allMovieDetails a la respuesta JSON - royalFilms.js

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const allMovieDetails = await scraper.getAllMovieDetails('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle, allMovieDetails }}, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
