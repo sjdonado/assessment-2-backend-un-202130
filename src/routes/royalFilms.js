0def9f0f8c3e9193298dbe6dc444b4b34fcd6278
Ubuntu <azure@back.kdpfqrmcvvpujdfpnztwpxkocd.bx.internal.cloudapp.net>
2021-09-10 03:47:15 +0000

    feat: allMoviesDetails

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,10 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const bodyMovies = await scraper.getPageMovies('https://royal-films.com/api/v1/movies/city/barranquilla/billboard?time=1631241126093');
+		const detallespeliculas = await scraper.getDetallesPeliculas(bodyMovies);
+		const allMoviesDetails = await scraper.getformat(detallespeliculas)
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,22 +1,71 @@
 const puppeteer = require('puppeteer');
-
+let browser;
+async function init() {
+	 browser = await puppeteer.launch();
+}
 /**
  * Go to url and return the page title
  * @param {string} url
  * @returns {string}
  */
 async function getPageTitle(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+	await init();
+	const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'networkidle0' });
 	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
 
-	await browser.close();
-
+	await page.close();
 	return title;
+}
+/**
+ * Go to url and return the page title
+ * @param {string} url
+ * @returns {JSON}
+ */
+async function getPageMovies(url) {
+	const page = await browser.newPage();
+  	await page.goto(url, { waitUntil: 'networkidle0' });
+	var bodyMovies = await page.evaluate(() => {return JSON.parse(document.querySelector('body').innerText)});
+
+	await page.close();
+	return bodyMovies;
+}
+var detallespeliculas=[];
+var formatodetalles=[];
+async function getDetallesPeliculas(bodyMovies) {
+
+	const page = await browser.newPage();
+	
+	for (let i = 0; i < bodyMovies.data.length; i++) {
+		var url='https://royal-films.com/api/v1/movie/'+bodyMovies.data[i].id+'/barranquilla?time=1631241126093';
+		await page.goto(url, { waitUntil: 'networkidle0' });
+		var detalles = await page.evaluate(() => {return JSON.parse(document.querySelector('body').innerText)});
+		detallespeliculas.push(detalles.data);
+	}
+	await browser.close();
+	return detallespeliculas;
+}
+async function getformat(detallespeliculas){
+	for (let i = 0; i < detallespeliculas.length; i++) {
+		var trailer = 'https://youtube.com/watch?v='+detallespeliculas[i].youtube
+		var pelicula={
+				originalTitle: detallespeliculas[i].original,
+				title: detallespeliculas[i].title,
+				synopsis: detallespeliculas[i].synopsis,
+				starred: detallespeliculas[i].starred,
+				director: detallespeliculas[i].director,
+				posterPhoto: detallespeliculas[i].poster_photo,
+				trailer: trailer
+		}	
+		formatodetalles.push(pelicula);
+	}
+	return formatodetalles;
 }
 
 module.exports = {
 	getPageTitle,
+	getPageMovies,
+	getDetallesPeliculas,
+	getformat
 };
