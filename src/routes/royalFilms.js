2e7406bed697cbd1cb5dac791b9d351fceddb538
DAMN21 <dmezae@uninorte.edu.co>
2021-09-10 00:07:59 +0000

    ci: :construction_workerSe creó la función getLinkM para obtenener las urls de las peliculas

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,7 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle } }, 200);
+		const pageData = await scraper.getLinkM('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle, pageData } }, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -17,6 +17,33 @@ async function getPageTitle(url) {
 	return title;
 }
 
+async function getLinkM(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
+  
+  await page.waitForSelector('[class=movie-box]')
+  const Pelis = await page.evaluate(() => { 
+	//const links=document.querySelector('[class="movie-box"]')['href'];
+	//const links=document.getElementsByClassName('[class="movie-box"]')[0].getAttribute('href');
+	const movies = document.querySelectorAll('[class="movie-box"]')
+	const urls = []
+	
+	movies.forEach((movies) => {
+	  link= movies.getAttribute("href")
+	  urls.push(link)
+  
+	  })
+	return urls	
+  })
+  
+  await browser.close();
+	return Pelis;   
+  }
+
+
+
 module.exports = {
 	getPageTitle,
+	getLinkM,
 };
