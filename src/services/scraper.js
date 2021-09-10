9d13099b7180af3ee22cac29e07c5cf493cac2d2
Ubuntu <azureuser@backend2130.tccy5hl1skyethzrkzl5qwjwrh.bx.internal.cloudapp.net>
2021-09-10 00:57:52 +0000

    feat: returns a valid response

--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,17 +1,78 @@
 const puppeteer = require('puppeteer');
-
+//const scraper = require('../services/scraper');
 /**
  * Go to url and return the page title
  * @param {string} url
  * @returns {string}
  */
+ async function getAllMoviesLinks(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+  
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('[class="movie-box"]');
+	const links = await page.evaluate(() => {
+        const elements = document.querySelectorAll('[class="movie-box"]');
+        const links = [];
+        for (let link of elements) {
+            links.push(link.href);
+        }
+        return links;
+    });
+  
+	  await browser.close();
+  
+	  return links;
+  }
+
+  async function Listing(url) {
+	
+ 	const links = await getAllMoviesLinks(url);
+  	const linksids=[];
+	links.forEach(element => {
+		linksids.push('https://royal-films.com/api/v1/movie/'+element.split('/')[5]+'/barranquilla')
+	});
+	
+	return getMoviesDetails(linksids);
+	
+}
+
+async function getMoviesDetails(urls) {
+	const moviesDetails = [];
+	try{
+		for(let url of urls){
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	const retorno = await page.evaluate(async (url) => {
+		const fet = await fetch(url);
+		const retorno = await fet.json();
+		return retorno;
+		},url);
+		moviesDetails.push({
+			originalTitle: retorno.data['original'],
+			title: retorno.data['title'],
+			synopsis: retorno.data['synopsis'],
+			starred: retorno.data['starred'],
+			director: retorno.data['director'],
+			posterPhoto: retorno.data['poster_photo'],
+			trailer: "https://youtube.com/watch?v="+retorno.data.youtube
+		}); 
+		await browser.close();
+	}
+	}catch(error){
+		console.log(error);
+	}
+	return moviesDetails;
+  }
+
 async function getPageTitle(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'networkidle0' });
 	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
-
+	
 	await browser.close();
 
 	return title;
@@ -19,4 +80,7 @@ async function getPageTitle(url) {
 
 module.exports = {
 	getPageTitle,
+	getAllMoviesLinks,
+	Listing,
+	getMoviesDetails,
 };
