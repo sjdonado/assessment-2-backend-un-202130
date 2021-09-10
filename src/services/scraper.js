694a403811b2f3d4fbe7f070129aa2638195d3b3
Alejandroo Gonzalez <aegs8002@gmail.com>
2021-09-10 02:31:38 +0000

    feat: agregar funcion getAllMovieDetails() - scrapper.js

--- src/services/scraper.js
+++ src/services/scraper.js
@@ -6,17 +6,81 @@ const puppeteer = require('puppeteer');
  * @returns {string}
  */
 async function getPageTitle(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.goto(url, { waitUntil: 'networkidle0' });
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	
+	await page.goto(url, { waitUntil: 'networkidle0' });
 	const title = await page.evaluate(() => document.querySelector('head > title').innerText);
-
 	await browser.close();
 
 	return title;
 }
 
+async function getLinks(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.waitForSelector('[class=movie-box]');
+
+	const enlaces = await page.evaluate(() => {
+		const elements = document.querySelectorAll('a.movie-box');
+		const links = [];
+		for (let element of elements) {
+			id = element.href;
+			id = id.substr(46, 4);
+			var link = 'https://royal-films.com/api/v1/movie/' + id + '/barranquilla?';
+			links.push(link);
+		}
+
+		return links;
+	});
+
+	await browser.close();
+
+	return enlaces;
+}
+
+async function getMovieDetails(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto(url);
+
+	  const movieDetails = await page.evaluate(async (url) => {
+		const movie = await fetch(url);
+		return movie.json();
+	  },url);
+
+	await browser.close(); 
+	
+	objMovieDetails = {
+		originalTitle: movieDetails.data['original'],
+		title: movieDetails.data['title'],
+		synopsis: movieDetails.data['synopsis'],
+		starred: movieDetails.data['starred'],
+		director: movieDetails.data['director'],
+		posterPhoto: movieDetails.data['poster_photo'],
+		trailer: "https://youtube.com/watch?v=" + movieDetails.data.youtube
+	};
+
+	return objMovieDetails;
+}
+
+async function getAllMovieDetails(url){
+
+	enlaces = await getLinks(url);
+	var allMovieDetails = [];
+
+	for(let enlace of enlaces){
+		var details = await getMovieDetails(enlace);
+		allMovieDetails.push(details);
+	}
+
+	return allMovieDetails;
+}
+
 module.exports = {
 	getPageTitle,
+	getAllMovieDetails,
 };
