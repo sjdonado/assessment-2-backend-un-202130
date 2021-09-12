7342bee0d2bc7685e6281862fd9774e80455299e
Egtorres17 <egtorres@uninorte.edu.co>
2021-09-12 15:19:57 -0500

    feat: modificando carpetas que no se habian guardado

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -1,31 +1,31 @@
 const scraper = require('../services/scraper');
 
 async function get(req, res) {
-    try {
-        const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-        const pageData = await scraper.getData('https://royal-films.com/cartelera/barranquilla');
+	try {
+		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
+		const pageData = await scraper.getData('https://royal-films.com/cartelera/barranquilla');
 
-        const datas=[]
-        
-        for (i of pageData) {
-            
-            x= scraper.data(i)
-            datas.push(x) 
-            
-          }
-          let values = await Promise.all(datas);
-        const dataJson = {
-            pageTitle: pageTitle,
-            allMoviesDetails:values
-        }
-        res.writeJSONResponse({ data: dataJson }, 200);
-    } catch(err) {
-        res.writeJSONResponse({ data: null, err: err.message }, 500);
-    }
+		const datas = []
+
+		for (i of pageData) {
+
+			x = scraper.data(i)
+			datas.push(x)
+
+		}
+		let values = await Promise.all(datas);
+		const dataJson = {
+			pageTitle: pageTitle,
+			allMoviesDetails: values
+		}
+		res.writeJSONResponse({ data: dataJson }, 200);
+	} catch (err) {
+		res.writeJSONResponse({ data: null, err: err.message }, 500);
+	}
 }
 
 module.exports = {
-    get,
+	get,
 };
 
 
