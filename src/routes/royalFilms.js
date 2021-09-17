18d5eec4db22e0b1c2784ffb9c101291fa1b9c6a
Alejandroo Gonzalez <aegs8002@gmail.com>
2021-09-17 14:36:25 +0000

    fix:cactualizar nombre de variable allMovieDetails

--- src/routes/royalFilms.js
+++ src/routes/royalFilms.js
@@ -3,8 +3,8 @@ const scraper = require('../services/scraper');
 async function get(req, res) {
 	try {
 		const pageTitle = await scraper.getPageTitle('https://royal-films.com/cartelera/barranquilla');
-		const allMovieDetails = await scraper.getAllMovieDetails('https://royal-films.com/cartelera/barranquilla');
-		res.writeJSONResponse({ data: { pageTitle, allMovieDetails }}, 200);
+		const allMoviesDetails = await scraper.getAllMovieDetails('https://royal-films.com/cartelera/barranquilla');
+		res.writeJSONResponse({ data: { pageTitle, allMoviesDetails }}, 200);
 	} catch(err) {
 		res.writeJSONResponse({ data: null, err: err.message }, 500);
 	}
